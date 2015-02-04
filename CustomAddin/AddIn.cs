f31a5e0cf8e85cb2f74929e68a6907aab1b42e84
Ron <ronan.mouquet@gmail.com>
2015-02-04 09:29:17 +0000

    Update AddIn.cs

--- CustomAddin/AddIn.cs
+++ CustomAddin/AddIn.cs
@@ -9,24 +9,6 @@ using ExcelDna.IntelliSense;
 
 namespace TestAddIn
 {
-    public static class TestFunctions
-    {
-        [ExcelFunction(Description = "A useful test function that adds two numbers, and returns the sum.")]
-        public static double AddThem(
-            [ExcelArgument(Name = "Augend", Description = "is the first number, to which will be added")] double v1,
-            [ExcelArgument(Name = "Addend", Description = "is the second number that will be added")]     double v2)
-        {
-            return v1 + v2;
-        }
-
-        [Description("Test function for the amazing Excel-DNA IntelliSense feature")]
-        public static string jDummyFunc()
-        {
-            return "Howzit !";
-        }
-
-    }
-
     public class AddIn : IExcelAddIn
     {
         private IntellisenseHelper _intellisense;
