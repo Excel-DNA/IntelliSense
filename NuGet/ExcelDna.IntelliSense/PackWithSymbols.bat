d14ef5c6d8801c96dd33f6d577f385127e9878ce
Govert van Drimmelen <govert@icon.co.za>
2018-07-01 23:47:30 +0200

    Add NuGet symbols package

---
+++ NuGet/ExcelDna.IntelliSense/PackWithSymbols.bat
@@ -0,0 +1,5 @@
+nuget pack ExcelDna.IntelliSense.nuspec -Symbols
+
+REM Now do
+REM nuget push ExcelDna.IntelliSense.x.x.x.nupkg -source https://nuget.org
+REM nuget push ExcelDna.IntelliSense.x.x.x.symbols.nupkg -source https://nuget.smbsrc.net/
