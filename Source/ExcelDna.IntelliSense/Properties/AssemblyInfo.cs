ee24925f84b928d30fe8cb52c51df84ba9336f67
Govert van Drimmelen <govert@icon.co.za>
2020-06-28 23:41:09 +0200

    Excel-DNA IntelliSense Version 1.3

--- Source/ExcelDna.IntelliSense/Properties/AssemblyInfo.cs
+++ Source/ExcelDna.IntelliSense/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Excel-DNA")]
 [assembly: AssemblyProduct("Excel-DNA IntelliSense")]
-[assembly: AssemblyCopyright("Copyright © Excel-DNA Contributors 2013-2019")]
+[assembly: AssemblyCopyright("Copyright © Excel-DNA Contributors 2013-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
