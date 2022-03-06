943882e64c96765452fc922f24c4bbd653768425
Govert van Drimmelen <govert@dnakode.com>
2022-03-06 12:04:59 +0200

    Update to v1.6.0-preview3

--- Source/ExcelDna.IntelliSense.Host/Properties/AssemblyInfo.cs
+++ Source/ExcelDna.IntelliSense.Host/Properties/AssemblyInfo.cs
@@ -1,17 +1,4 @@
-using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Excel-DNA IntelliSense Host")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Excel-DNA")]
-[assembly: AssemblyProduct("Excel-DNA IntelliSense")]
-[assembly: AssemblyCopyright("Copyright © Excel-DNA Contributors 2013-2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,17 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("03312a7e-e3c3-4201-9f2a-8a68b6ab9bca")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.0")]
-[assembly: AssemblyFileVersion("1.5.1.0")]
-[assembly: AssemblyInformationalVersion("1.5.1")]
--- Source/ExcelDna.IntelliSense/Properties/AssemblyInfo.cs
+++ Source/ExcelDna.IntelliSense/Properties/AssemblyInfo.cs
@@ -1,17 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Excel-DNA IntelliSense")]
-[assembly: AssemblyDescription("IntelliSense service for Microsoft Excel")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Excel-DNA")]
-[assembly: AssemblyProduct("Excel-DNA IntelliSense")]
-[assembly: AssemblyCopyright("Copyright © Excel-DNA Contributors 2013-2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,17 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7b77ea41-110a-4940-aab3-95b3fd73f6ec")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.0")]
-[assembly: AssemblyFileVersion("1.5.1.0")]
-[assembly: AssemblyInformationalVersion("1.5.1")]
