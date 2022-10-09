67bb5fe55c290274dc74996dcefe6ec0fb51d3df
Govert van Drimmelen <govert@dnakode.com>
2022-10-10 00:32:25 +0200

    Update to v1.6.1-alpha2

--- NuGet/push.cmd
+++ NuGet/push.cmd
@@ -5,5 +5,5 @@ set currentPath=%~dp0
 set basePath=%currentPath:~0,-1%
 set outputPath=%basePath%\nupkg
 
-nuget.exe push "%outputPath%\ExcelDna.IntelliSense.1.6.1-alpha.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
+nuget.exe push "%outputPath%\ExcelDna.IntelliSense.1.6.1-alpha2.nupkg" -Source  https://api.nuget.org/v3/index.json -Verbosity detailed -NonInteractive
 @if errorlevel 1 goto end
